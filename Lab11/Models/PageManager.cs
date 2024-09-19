0968ce9f7b456a551cebafb1690ace124b3b5414
va.sokolov <sokolov_vadim95@bk.ru>
2024-09-19 10:56:13 +0300

    Добавлен PageManager, EnterPage

---
+++ Lab11/Models/PageManager.cs
@@ -0,0 +1,27 @@
+﻿using Lab11.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Lab11.Models
+{
+    class PageManager
+    {
+        public Page CurrentPage { get; set; }
+
+        public PageManager() { }
+
+        public PageManager(Page currentPage)
+        {
+            CurrentPage = currentPage;
+        }
+
+        public void SetCurrentPage(Page page)
+        {
+            CurrentPage = page;
+        }
+    }
+}
--- Lab11/ViewModels/MainViewModel.cs
+++ Lab11/ViewModels/MainViewModel.cs
@@ -1,13 +1,43 @@
-﻿using System;
+﻿using Lab11.Models;
+using Lab11.Views;
+using Prism.Commands;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace Lab11.ViewModels
 {
     class MainViewModel : BindableBase
     {
+        private PageManager _pageManager;
+
+        public MainViewModel()
+        {
+            _pageManager = new PageManager(new EnterPage());
+        }
+
+        public Page CurrentPage
+        {
+            get { return _pageManager.CurrentPage; }
+            set
+            {
+                _pageManager.CurrentPage = value;
+                RaisePropertyChanged(nameof(CurrentPage));
+            }
+        }
+
+        private DelegateCommand enterCommand;
+        public ICommand EnterCommand => enterCommand ??= new DelegateCommand(Enter);
+
+        private void Enter()
+        {
+            _pageManager.SetCurrentPage(new EnterPage());
+            RaisePropertyChanged(nameof(CurrentPage));
+        }
 
     }
 }
