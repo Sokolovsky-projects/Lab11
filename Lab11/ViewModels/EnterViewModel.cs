507e5737bce99772705003111ed6855ac96dd4c8
va.sokolov <sokolov_vadim95@bk.ru>
2024-09-19 21:47:31 +0300

    Стиль кнопок выделен в отдельный файл, добавленны наброски страницы входаю

---
+++ Lab11/ViewModels/EnterViewModel.cs
@@ -0,0 +1,43 @@
+﻿using Prism.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Lab11.ViewModels
+{
+    class EnterViewModel : BindableBase
+    {
+        private string _login;
+        public string Login 
+        {
+            get { return _login; } 
+            set 
+            {
+                _login = value; 
+                RaisePropertyChanged(nameof(Login));
+            }
+        }
+
+        private string _password;
+        public string Password
+        {
+            get { return _password; }
+            set
+            {
+                _password = value;
+                RaisePropertyChanged(nameof(Password));
+            }
+        }
+
+        private DelegateCommand authorizeCommand;
+        public ICommand AuthorizeCommand => authorizeCommand ??= new DelegateCommand(Authorize);
+
+        private void Authorize()
+        {
+        }
+
+    }
+}
